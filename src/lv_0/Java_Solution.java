2473474238fe519336c615a3767a3a718d3764fe
song-eunhye <dmsgp8292@gmail.com>
2023-10-31 17:30:03 +0900

    프로그래머스 코테 LV.0 a와 b출력하기

--- src/lv_0/Java_Solution.java
+++ src/lv_0/Java_Solution.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class Java_Solution {
     // 문자열 출력하기
     // 답 1.
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String a = sc.next();
+//        if (1 <= a.length() && a.length() <= 1000000){
+//            System.out.println(a);
+//        }
+//    }
+
+    //a와 b 출력하기
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        if (1 <= a.length() && a.length() <= 1000000){
-            System.out.println(a);
-        }
-    }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
+        System.out.println("a = " + a + "\nb = " + b );
+    }
 }
+
--- src/lv_0/Kotlin_Solution.kt
+++ src/lv_0/Kotlin_Solution.kt
@@ -1,26 +1,44 @@
 package lv_0
 
-class Kotlin_Solution {
-    // 문자열 출력하기
-    // 답 1.
+class Kotlin_Solution {}
+
+// 문자열 출력하기
+//// 답 1.
+//fun main(args: Array<String>) {
+//    println(readLine(args)!!)
+//}
+//
+//fun readLine(args: Array<String>): String? {
+//    return args.joinToString()
+//}
+//// 답 2.
 //    fun main(args: Array<String>) {
-//        println(readLine(args)!!)
-//    }
-//    fun readLine(args: Array<String>): String? {
-//        return args.joinToString()
-//    }
-    // 답 2.
-    fun main(args: Array<String>) {
-        readlnOrNull()?.let (::println)
+//        readlnOrNull()?.let (::println)
 //        //답 2-2.
 //        readLine()?.let(::println)
-    }
-    /**
-     * readLine() : Kotlin의 표준 라이브러리 함수. 콘솔의 입력값을 읽어올 때 사용한다. 자바의 Scanner 랑 비슷한 것 같다.
-     * -> 1.6.0 부터 readln() 이 새로 추가되었다. readLine() 은 점차 사용하지 않을 것으로 추측된다.
-     * readln() : 1.6.0 부터 추가된 Kotlin의 표준 라이브러리 함수.  콘솔의 입력값을 읽어올 때 사용한다. readLine()!! 과 같다. not null 임.
-     * readlnOrNull() : 1.6.0 부터 추가된 Kotlin의 표준 라이브러리 함수. 콘솔의 입력값을 읽어올 때 사용한다. readLine() 과 같다.
-     * https://soopeach.tistory.com/44
-     */
+//    }
+/**
+ * readLine() : Kotlin의 표준 라이브러리 함수. 콘솔의 입력값을 읽어올 때 사용한다. 자바의 Scanner 랑 비슷한 것 같다.
+ * -> 1.6.0 부터 readln() 이 새로 추가되었다. readLine() 은 점차 사용하지 않을 것으로 추측된다.
+ * readln() : 1.6.0 부터 추가된 Kotlin의 표준 라이브러리 함수.  콘솔의 입력값을 읽어올 때 사용한다. readLine()!! 과 같다. not null 임.
+ * readlnOrNull() : 1.6.0 부터 추가된 Kotlin의 표준 라이브러리 함수. 콘솔의 입력값을 읽어올 때 사용한다. readLine() 과 같다.
+ * https://soopeach.tistory.com/44
+ */
 
+
+//a와 b 출력하기
+fun main(args: Array<String>) {
+//        답 1.
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+////        답 2.
+//    readlnOrNull()?.let {
+//        val (a, b) = it.split(' ').map(String::toInt)
+//        println("a = $a")
+//        println("b = $b")
+//    }
 }
+
+
+
+
+
