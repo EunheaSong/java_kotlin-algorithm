1ff8e62a219813ee523eff87920764c8bec1a1cc
song-eunhye <dmsgp8292@gmail.com>
2024-01-04 09:51:27 +0900

    GroupAnagrams

---
+++ src/algorithm/java/leetcode/medium/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package algorithm.java.leetcode.medium;
+
+import java.util.List;
+
+public class GroupAnagrams {
+    /*
+        System.out.println("49. Group Anagrams : ");
+
+        Example 1:
+        Input: strs = ["eat","tea","tan","ate","nat","bat"]
+        Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+        Example 2:
+        Input: strs = [""]
+        Output: [[""]]
+
+        Example 3:
+        Input: strs = ["a"]
+        Output: [["a"]]
+     */
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+}
