dae17037ea56a868825a6b1a56be671749a6e3c2
song-eunhye <dmsgp8292@gmail.com>
2024-01-26 19:14:53 +0900

    배열 두 배 만들기 - 타인의 답

--- src/algorithm/java/programmers/lv0/배열_두_배_만들기.java
+++ src/algorithm/java/programmers/lv0/배열_두_배_만들기.java
@@ -1,5 +1,7 @@
 package algorithm.java.programmers.lv0;
 
+import java.util.Arrays;
+
 public class 배열_두_배_만들기 {
 
     public int[] solution(int[] numbers) {
@@ -8,5 +10,16 @@ public class 배열_두_배_만들기 {
             numbers[i] = n * 2;
         }
         return numbers;
+    }
+
+    static class OtherPeopleAnswer {
+        public int[] solution(int[] numbers) {
+            return Arrays.stream(numbers).map(i -> i * 2).toArray();
+        }
+
+        public int[] solution2(int[] numbers) {
+            for(int i=0; i<numbers.length; i++) numbers[i] *=2;
+            return numbers;
+        }
     }
 }
