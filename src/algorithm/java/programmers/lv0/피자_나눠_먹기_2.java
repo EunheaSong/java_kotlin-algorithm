e16e936f470b0313d931a753483e475e85447cdc
song-eunhye <dmsgp8292@gmail.com>
2024-01-31 23:39:09 +0900

    피자 나눠 먹기2

--- src/algorithm/java/programmers/lv0/피자_나눠_먹기_2.java
+++ src/algorithm/java/programmers/lv0/피자_나눠_먹기_2.java
@@ -3,7 +3,12 @@ package algorithm.java.programmers.lv0;
 public class 피자_나눠_먹기_2 {
 
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
+        int pizza = 6;
+        while (pizza % n > 0) {
+            pizza += 6;
+            answer++;
+        }
         return answer;
     }
 }
