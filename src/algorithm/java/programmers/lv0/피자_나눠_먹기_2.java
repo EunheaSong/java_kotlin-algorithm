f4462e2170297d38e0c1f08e566efe60b40b1b09
song-eunhye <dmsgp8292@gmail.com>
2024-01-31 23:40:39 +0900

    피자 나눠 먹기2 - 타인의 답

--- src/algorithm/java/programmers/lv0/피자_나눠_먹기_2.java
+++ src/algorithm/java/programmers/lv0/피자_나눠_먹기_2.java
@@ -11,4 +11,32 @@ public class 피자_나눠_먹기_2 {
         }
         return answer;
     }
+
+    static class OtherPeopleAnswer {
+
+        public int GCD(int num1, int num2) {
+            if (num1 % num2 == 0)
+                return num2;
+            return GCD(num2, num1 % num2);
+        }
+
+        public int LCM(int num1, int num2) {
+            return num1 * num2 / GCD(num1, num2);
+        }
+
+        public int solution(int n) {
+            return LCM(n, 6) / 6;
+        }
+
+
+        public int solution2(int n) {
+            int answer = 1;
+            while (true) {
+                if(6*answer%n==0) break;
+                answer++;
+            }
+            return answer;
+        }
+
+    }
 }
