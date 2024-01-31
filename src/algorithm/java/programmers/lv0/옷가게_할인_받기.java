5b9d5aade5a75d1908fff0080a828409b294c16f
song-eunhye <dmsgp8292@gmail.com>
2024-01-31 23:43:56 +0900

    옷가게 할인 받기 - 타인의 답

--- src/algorithm/java/programmers/lv0/옷가게_할인_받기.java
+++ src/algorithm/java/programmers/lv0/옷가게_할인_받기.java
@@ -13,4 +13,32 @@ public class 옷가게_할인_받기 {
         }
         return answer;
     }
+
+    static class OtherPeopleAnswer {
+
+        public int solution(int price) {
+            int answer = 0;
+
+            if(price>=500000) return (int)(price*0.8);
+            if(price>=300000) return (int)(price*0.9);
+            if(price>=100000) return (int)(price*0.95);
+
+            return price;
+        }
+
+        public int solution2(int price) {
+            int answer = 0;
+
+            if(price>=100000 && price < 300000){
+                answer = price * 95 / 100;
+            }else if(price>=200000 && price < 500000){
+                answer = price * 90 / 100;
+            }else if(price>=500000){
+                answer = price * 80 / 100;
+            }else{
+                answer = price;
+            }
+            return answer;
+        }
+    }
 }
