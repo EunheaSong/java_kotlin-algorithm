d45a8c0caf9d86952ba669e7854dbf4190af0d1a
song-eunhye <dmsgp8292@gmail.com>
2024-02-04 20:17:39 +0900

    진료순서_정하 - 타인의 답

--- src/algorithm/java/programmers/lv0/진료순서_정하기.java
+++ src/algorithm/java/programmers/lv0/진료순서_정하기.java
@@ -1,8 +1,10 @@
 package algorithm.java.programmers.lv0;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class 진료순서_정하기 {
 
@@ -28,5 +30,49 @@ public class 진료순서_정하기 {
             answer[i] = j;
         }
         return answer;
+    }
+
+    public static class OtherPeopleAnswer {
+        /*
+        테스트 1 〉	통과 (5.64ms, 74.3MB)
+        테스트 2 〉	통과 (4.14ms, 73.4MB)
+        테스트 3 〉	통과 (6.47ms, 72.1MB)
+        테스트 4 〉	통과 (4.41ms, 76.6MB)
+        테스트 5 〉	통과 (5.89ms, 78.9MB)
+        테스트 6 〉	통과 (3.73ms, 71.2MB)
+        테스트 7 〉	통과 (4.90ms, 73MB)
+        테스트 8 〉	통과 (3.85ms, 75.1MB)
+         */
+        public int[] solution(int[] e) {
+            return Arrays.stream(e).map(i -> Arrays.stream(e).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList()).indexOf(i) + 1).toArray();
+        }
+
+        /*
+        테스트 1 〉	통과 (0.02ms, 74MB)
+        테스트 2 〉	통과 (0.02ms, 73.7MB)
+        테스트 3 〉	통과 (0.02ms, 75.7MB)
+        테스트 4 〉	통과 (0.01ms, 72.9MB)
+        테스트 5 〉	통과 (0.02ms, 80.5MB)
+        테스트 6 〉	통과 (0.01ms, 77.3MB)
+        테스트 7 〉	통과 (0.03ms, 76.5MB)
+        테스트 8 〉	통과 (0.03ms, 73.6MB)
+         */
+        public int[] solution2(int[] emergency) {
+            int[] answer = new int[emergency.length];
+
+            for(int i = 0; i < answer.length; i++){
+                if(answer[i] != 0){
+                    continue;
+                }
+                int idx = 1;
+                for(int j = 0; j < answer.length; j++){
+                    if(emergency[i] < emergency[j]){
+                        idx++;
+                    }
+                }
+                answer[i] = idx;
+            }
+            return answer;
+        }
     }
 }
