5979fc8013baa7b799e9e89e47f1e4d2dc0369b1
song-eunhye <dmsgp8292@gmail.com>
2024-02-07 22:12:45 +0900

    연속된 수의 - 타인의 답

--- src/algorithm/java/programmers/lv0/연속된_수의_합.java
+++ src/algorithm/java/programmers/lv0/연속된_수의_합.java
@@ -28,4 +28,41 @@ public class 연속된_수의_합 {
         }
         return answer;
     }
+
+    public static class OtherPeopleAnswer {
+        public int[] solution(int num, int total) {
+            int[] answer = new int[num];
+            int check = num*(num+1) / 2;
+            int start = (total - check) / num + 1;
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = start + i ;
+            }
+            return answer;
+        }
+
+        public int[] solution2(int num, int total) {
+            int[] answer = new int[num];
+            int mid = total / num;
+            int m = (num - 1) / 2;
+            for(int i = 0; i < num; i++) {
+                answer[i] = mid + i - m;
+            }
+            return answer;
+        }
+
+        public int[] solution3(int num, int total) {
+            int[] answer = new int[num];
+            int temp = 0;
+            for(int i=0;i<num;i++){
+                temp+=i;
+            }
+            int value = (total-temp)/num;
+
+            for (int i=0;i<num;i++) {
+                answer[i]=i+value;
+            }
+
+            return answer;
+        }
+    }
 }
