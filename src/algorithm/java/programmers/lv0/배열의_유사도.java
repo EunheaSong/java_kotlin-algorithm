f3977f83948e0051e2e0ece2adc4c780ff3de197
song-eunhye <dmsgp8292@gmail.com>
2024-02-09 23:38:54 +0900

    배열의 유사 - 타인의 답

--- src/algorithm/java/programmers/lv0/배열의_유사도.java
+++ src/algorithm/java/programmers/lv0/배열의_유사도.java
@@ -1,5 +1,9 @@
 package algorithm.java.programmers.lv0;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class 배열의_유사도 {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
@@ -12,5 +16,12 @@ public class 배열의_유사도 {
             }
         }
         return answer;
+    }
+
+    public static class OtherPeopleAnswer {
+        public int solution(String[] s1, String[] s2) {
+            Set<String> set = new HashSet<>(Arrays.asList(s1));
+            return (int)Arrays.stream(s2).filter(set::contains).count();
+        }
     }
 }
