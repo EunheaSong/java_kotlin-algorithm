f9e84af4894a1fcca086bfe3d961087b7b17e199
song-eunhye <dmsgp8292@gmail.com>
2024-02-20 13:43:10 +0900

    v.kotlin GroupAnagrams

---
+++ src/algorithm/kotlin/leetcode/medium/GroupAnagrams.kt
@@ -0,0 +1,30 @@
+package algorithm.kotlin.leetcode.medium
+
+class GroupAnagrams {
+    /*
+    49. Group Anagrams
+     */
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+         val map: HashMap<String, List<String>> = hashMapOf()
+         strs.forEach {
+             val s = it.toCharArray().sorted().joinToString()
+             if (!map.containsKey(s)) {
+                 map[s] = listOf(it)
+             } else {
+                 map[s] = map[s]!!.plus(it)
+             }
+         }
+         return map.map { it.value }
+    }
+
+    fun groupAnagrams2(strs: Array<String>): List<List<String>> {
+        val map: HashMap<String, List<String>> = hashMapOf()
+        strs.forEach {
+            val s = it.toCharArray().sorted().joinToString()
+            map[s] = listOf(it).plus(map.getOrDefault(s, mutableListOf()))
+        }
+        return map.map { it.value }
+    }
+
+}
