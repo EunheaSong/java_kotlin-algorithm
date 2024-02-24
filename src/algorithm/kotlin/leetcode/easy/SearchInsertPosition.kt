8fe318d9d4915b00dc58c9340753b3ab107a066d
song-eunhye <dmsgp8292@gmail.com>
2024-02-24 10:05:00 +0900

    v.kotlin - Search Insert Position

---
+++ src/algorithm/kotlin/leetcode/easy/SearchInsertPosition.kt
@@ -0,0 +1,19 @@
+package algorithm.kotlin.leetcode.easy
+
+class SearchInsertPosition {
+    /*
+    35. Search Insert Position
+     */
+
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        nums.forEachIndexed { index, num ->
+            if (target < num || num == target) return index
+            else if (
+                index == nums.size-1 ||
+                nums[0] < target && target < nums[index + 1]
+            ) return index + 1
+        }
+        return -1
+    }
+
+}
