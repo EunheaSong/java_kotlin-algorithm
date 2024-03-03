70689b104edd053e707b9ed8e304af4597f708cc
song-eunhye <dmsgp8292@gmail.com>
2024-03-03 23:58:10 +0900

    Plus One

--- src/algorithm/java/leetcode/easy/PlusOne.java
+++ src/algorithm/java/leetcode/easy/PlusOne.java
@@ -5,22 +5,36 @@ public class PlusOne {
     66. Plus One
      */
 
-    public int[] plusOne(int[] digits) {
-        int addInt = 0;
-        int j = 1;
-        for (int i = digits.length-1; 0 <= i; i--) {
-            addInt += digits[i] * j;
-            System.out.println();
-            j *= 10;
-        }
-        addInt += 1;
-        String a = Integer.toString(addInt);
-        char[] c = a.toCharArray();
-        int[] newDigits = new int[c.length];
-        for (int i = 0; i < c.length-1; i++) {
-            newDigits[i] = c[i] - 49;
-        }
-        return newDigits;
-    }
+    //데이터 범위가 초과되어 실패.
+//    public int[] plusOne(int[] digits) {
+//        int addInt = 0;
+//        int j = 1;
+//        for (int i = digits.length - 1; 0 <= i; i--) {
+//            addInt += digits[i] * j;
+//            System.out.println();
+//            j *= 10;
+//        }
+//        addInt += 1;
+//        String a = Integer.toString(addInt);
+//        char[] c = a.toCharArray();
+//        int[] newDigits = new int[c.length];
+//        for (int i = 0; i < c.length - 1; i++) {
+//            newDigits[i] = c[i] - 49;
+//        }
+//        return newDigits;
+//    }
 
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
 }
