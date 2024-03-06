9df5abf4d783d8544d6018766b5878bf8ae6f7e3
song-eunhye <dmsgp8292@gmail.com>
2024-03-06 09:38:57 +0900

    Linked List Cycle - 타인의 답

--- src/algorithm/java/leetcode/easy/LinkedListCycle.java
+++ src/algorithm/java/leetcode/easy/LinkedListCycle.java
@@ -38,4 +38,20 @@ public class LinkedListCycle {
         }
     }
 
+    static class LeetCodeAnswer {
+
+        public boolean hasCycle(ListNode head) {
+            if (head == null) return false;
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
