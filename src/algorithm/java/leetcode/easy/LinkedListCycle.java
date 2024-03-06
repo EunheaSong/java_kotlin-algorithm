a7672842fae6299c95821fc2409b610cf1f6d1ef
song-eunhye <dmsgp8292@gmail.com>
2024-03-06 09:38:47 +0900

    Linked List Cycle

---
+++ src/algorithm/java/leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,41 @@
+package algorithm.java.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinkedListCycle {
+    /*
+    141. Linked List Cycle
+     */
+
+    // Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Map<ListNode, Integer> m = new HashMap();
+        if (head == null) return false;
+        return cycle(head, m);
+
+    }
+
+    public boolean cycle(ListNode head, Map<ListNode, Integer> m) {
+        if (head.next != null) {
+            if (m.containsKey(head.next)) {
+                return true;
+            }
+            m.put(head.next, head.val);
+            return cycle(head.next, m);
+        } else {
+            return false;
+        }
+    }
+
+}
