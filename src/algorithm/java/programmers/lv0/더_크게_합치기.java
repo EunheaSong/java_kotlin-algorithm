ab98e45326aa96a97530265ec6fda7939ff12805
song-eunhye <dmsgp8292@gmail.com>
2024-03-18 09:56:07 +0900

    프로그래머스 - 두 수의 연산 값으로 합치기

--- src/algorithm/java/programmers/lv0/더_크게_합치기.java
+++ src/algorithm/java/programmers/lv0/더_크게_합치기.java
@@ -10,4 +10,12 @@ public class 더_크게_합치기 {
             return Integer.parseInt(s2);
         }
     }
+
+    public class 두_수의_연산값으로_합치기 {
+        public int solution(int a, int b) {
+            int i1= Integer.parseInt("" + a + b);
+            int i2 = 2 * a * b;
+            return Math.max(i1, i2);
+        }
+    }
 }
