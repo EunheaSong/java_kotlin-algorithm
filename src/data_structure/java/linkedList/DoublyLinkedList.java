0a8a31079b657ca9152af2eb76a7571f46ade890
송은혜 <dmsgp8292@gmail.com>
2024-04-23 15:17:31 +0900

    DoublyLinkedList - append(),removeLast(),prepend(),removeFirst(),get()

--- src/data_structure/java/linkedList/DoublyLinkedList.java
+++ src/data_structure/java/linkedList/DoublyLinkedList.java
@@ -5,8 +5,8 @@ public class DoublyLinkedList {
     private Node tail;
     private int length;
 
-    class Node {
-        int value;
+    public class Node {
+        public int value;
         Node next;
         Node prev;
 
@@ -20,6 +20,76 @@ public class DoublyLinkedList {
         head = node;
         tail = node;
         length = 1;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+            newNode.next = head;
+        }
+        head = newNode;
+        length++;
+    }
+
+    public Node removeFirst() {
+        if(length == 0) return null;
+        Node temp = head;
+        if(length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || length <= index) return null;
+        Node temp = head;
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length -1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
     }
 
     public void printList() {
