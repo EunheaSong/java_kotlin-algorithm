2f177a14c1ca21968ab0e18a93856f5fe366d82d
송은혜 <dmsgp8292@gmail.com>
2024-04-24 15:34:46 +0900

    DoublyLinkedList - set(), insert(), remove()

--- src/data_structure/java/linkedList/DoublyLinkedList.java
+++ src/data_structure/java/linkedList/DoublyLinkedList.java
@@ -92,6 +92,51 @@ public class DoublyLinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node before = get(index - 1);
+        Node newNode = new Node(value);
+        Node after = before.next;
+        newNode.prev = before;
+        newNode.next = before.next;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length-1) return removeLast();
+        Node temp = get(index);
+        Node before = temp.prev;
+        Node after = temp.next;
+        before.next = after;
+        after.prev = before;
+        temp.prev = null;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while(temp != null) {
