3fa457de5c5ee9c8746a4561a912baefe7a55d08
송은혜 <dmsgp8292@gmail.com>
2024-04-30 10:58:54 +0900

    Graph

---
+++ src/data_structure/java/graph/Main.java
@@ -0,0 +1,25 @@
+package data_structure.java.graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.printGraph();
+        System.out.println("---------");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.printGraph();
+        System.out.println("---------");
+
+        graph.remove("A", "B");
+        graph.printGraph();
+        System.out.println("---------");
+
+        graph.removeVertex("A");
+        graph.printGraph();
+        System.out.println("---------");
+    }
+}
