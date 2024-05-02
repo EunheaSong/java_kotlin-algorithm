46c2aec0c1e51d40edf7700d4e780ff49a5c59f9
송은혜 <dmsgp8292@gmail.com>
2024-05-02 16:39:19 +0900

    recursion을 활용한 BST.contains(),insert(),delete()

--- src/data_structure/java/binarySearchTree/BinarySearchTree.java
+++ src/data_structure/java/binarySearchTree/BinarySearchTree.java
@@ -1,7 +1,7 @@
 package data_structure.java.binarySearchTree;
 
 public class BinarySearchTree {
-    private Node root;
+    public Node root; //코드 확인 테스트를 위해 public으로 변경.
 
     class Node {
         int value;
@@ -52,5 +52,71 @@ public class BinarySearchTree {
             }
         }
         return false;
+    }
+
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null) return false;
+        if (value == currentNode.value) return true;
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+        if (value < currentNode.value) {
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            //value == currentNode.value. 현재 노드를 null 로 만들어야한다.
+            if (currentNode.left == null && currentNode.right == null) {
+                currentNode = null;
+            } else if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            } else if (currentNode.right == null) {
+                currentNode = currentNode.left;
+            } else {
+                // 둘 다 있을 때.
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                //우측에서 찾은 min 노드를 제거하고 재정렬해준다.
+                currentNode.right = deleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
+    private int minValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    //특정 값을 제거하는 메서드
+    public void deleteNode(int value) {
+        deleteNode(root, value);
     }
 }
--- src/data_structure/java/binarySearchTree/Main.java
+++ src/data_structure/java/binarySearchTree/Main.java
@@ -7,6 +7,42 @@ public class Main {
         bst.insert(50);
         bst.insert(25);
 
-        System.out.println(bst.contains(30));
+        System.out.println("Contains 30 : " + bst.contains(30));
+        System.out.println("Contains 40 : " + bst.contains(40));
+        System.out.println("rContains 25 : " + bst.rContains(25));
+
+        bst.rInsert(28);
+        System.out.println("rContains 28 : " + bst.rContains(28));
+
+        BinarySearchTree bst2 = new BinarySearchTree();
+        bst2.rInsert(50);
+        bst2.rInsert(25);
+        bst2.rInsert(67);
+        System.out.println(" " + bst2.root.value);
+        System.out.println(bst2.root.left.value + " " + bst2.root.right.value);
+
+        bst.insert(45);
+        bst.insert(19);
+        /*
+        현재 트리
+                30
+             25    50
+           19 28  45
+         */
+
+        bst.deleteNode(30);
+        /*
+        30 제거 후 예상 트리
+                45
+             25    50
+           19 28  45
+         */
+        //루트 노드 확인.
+        System.out.println("Root : " + bst.root.value);
+        System.out.println("Root.left : " + bst.root.left.value + " Root.right : " + bst.root.right.value);
+        System.out.println("Root.left.left : " + bst.root.left.left.value + " Root.left.right : " + bst.root.left.right.value);
+        System.out.println("Root.right.left : " + bst.root.right.left + " Root.right.right : " + bst.root.right.right);
+        System.out.println("기존 45가 있던 자리 : " + bst.root.right.left);
+
     }
 }
