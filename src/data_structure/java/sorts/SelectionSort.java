5fb67f92055e95541dfd8026f9c8dbc7fc52b661
송은혜 <dmsgp8292@gmail.com>
2024-05-03 16:04:59 +0900

    Sort Algorithm - bubble, insertion, selection

---
+++ src/data_structure/java/sorts/SelectionSort.java
@@ -0,0 +1,27 @@
+package data_structure.java.sorts;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {5, 3, 2, 1, 4};
+        selectionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
