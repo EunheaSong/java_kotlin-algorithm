dc132ee166b26c2475ca8af1a272c683042267fd
송은혜 <dmsgp8292@gmail.com>
2024-05-07 17:40:48 +0900

    QuickSort

---
+++ src/algorithm/sorts/QuickSort.java
@@ -0,0 +1,41 @@
+package algorithm.sorts;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    public static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+        return swapIndex;
+    }
+
+    public static void quickSortHelper(int[] array, int left, int right) {
+        if (left < right) {
+            int pivot = pivot(array, left, right);
+            quickSortHelper(array, left, pivot-1);
+            quickSortHelper(array, pivot+1, right);
+        }
+    }
+
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length-1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4,6,1,7,3,2,5};
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
